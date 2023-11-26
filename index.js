40a00872e1978d523fa68fccb3480dc182f5a7fa
Alex Jason <mac@MacBook-Pro-de-Alex.local>
2023-11-26 03:13:30 -0300

    Fix bugs

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
  * @format
  */
 
+import React from 'react';
+
 import {AppRegistry} from 'react-native';
-import { ThemeProvider } from 'styled-components';
+import {ThemeProvider} from 'styled-components';
 import App from './src/components/App';
-import {name as appName} from './app.json';
 
 import theme from './src/assets/styles/default';
 
 export default function Main() {
-    return (
-        <ThemeProvider theme={theme}>
-            <App />
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  );
 }
 
-AppRegistry.registerComponent(appName, () => Main)
+AppRegistry.registerComponent('Verbalista', () => Main);
